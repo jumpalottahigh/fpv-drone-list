220356169ae6e49f2510c248e52338bcdc0f7256
jumpalottahigh <georgiyanev.gy@gmail.com>
2020-02-15 12:17:31 +0200

    Implement simple search

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # FPV Drone List
 
+https://jumpalottahigh.github.io/fpv-drone-list
 https://fpv-drone-list.netlify.com
+
+Compare BNF and PNP quadcopters' motor size, KV, prop size, weight, class and battery size.
+
+Based on data from [Joshua Bardwell's spreadsheet](https://www.youtube.com/watch?v=5Aoj4N0V1V4).
+
+All links are untampered and left as is - affiliate links that would support Joshua Bardwell's channel if clicked before making a purchase.
+
+Site built by Georgi Yanev.
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,51 +1,118 @@
 import React from 'react'
+import { DebounceInput } from 'react-debounce-input'
+
+const TableHeadings = () => (
+  <tr>
+    <th>Make</th>
+    <th>Model</th>
+    <th>Class</th>
+    <th>Prop diameter</th>
+    <th>Motor size</th>
+    <th>Motor KV</th>
+    <th>Battery voltage</th>
+    <th>Battery mAh</th>
+    <th>Dry weight(g)</th>
+    <th>Product page</th>
+  </tr>
+)
+
+const TableBody = ({ data }) => (
+  <tbody>
+    {data.map(drone => (
+      <tr key={drone.id}>
+        <td>{drone.Make}</td>
+        <td>{drone.Model}</td>
+        <td>{drone.Class}</td>
+        <td>{drone.Prop_diameter}</td>
+        <td>{drone.Motor_size}</td>
+        <td>{drone.Motor_kv}</td>
+        <td>{drone.Battery_voltage}</td>
+        <td>{drone.Battery_mAh}</td>
+        <td>{drone.Dry_Weight__g_}</td>
+        <td>
+          <a
+            href={drone.field10}
+            target="_blank"
+            rel="noopener noreferrer"
+            title={drone.field10}
+          >
+            Link
+          </a>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+)
 
 export default function Table({ data }) {
-  const TableHeadings = (
-    <tr>
-      <th>Make</th>
-      <th>Model</th>
-      <th>Class</th>
-      <th>Prop diameter</th>
-      <th>Motor size</th>
-      <th>Motor KV</th>
-      <th>Battery voltage</th>
-      <th>Battery mAh</th>
-      <th>Dry weight(g)</th>
-      <th>Product page</th>
-    </tr>
-  )
+  const [isFiltered, setIsFiltered] = React.useState(false)
+  const [filteredData, setFilteredData] = React.useState([])
+  const [searchedValue, setSearchedValue] = React.useState('')
+
+  const handleSearch = e => {
+    const searchedValue = e?.target?.value
+
+    if (!searchedValue) {
+      setIsFiltered(false)
+    }
+
+    const filtered = data.filter(item => {
+      let found = false
+
+      Object.values(item).forEach(v => {
+        if (
+          v
+            .toLowerCase()
+            .trim()
+            .includes(searchedValue.toLowerCase().trim())
+        ) {
+          found = true
+          return
+        }
+      })
+
+      if (found) {
+        return item
+      } else {
+        return
+      }
+    })
+
+    setFilteredData(filtered)
+    setIsFiltered(true)
+    setSearchedValue(searchedValue)
+  }
 
   return (
-    <table class="table">
-      <thead>{TableHeadings}</thead>
-      <tfoot>{TableHeadings}</tfoot>
-      <tbody>
-        {/* <tr class="is-selected"> */}
-        {data.map(drone => (
-          <tr key={drone.id}>
-            <td>{drone.Make}</td>
-            <td>{drone.Model}</td>
-            <td>{drone.Class}</td>
-            <td>{drone.Prop_diameter}</td>
-            <td>{drone.Motor_size}</td>
-            <td>{drone.Motor_kv}</td>
-            <td>{drone.Battery_voltage}</td>
-            <td>{drone.Battery_mAh}</td>
-            <td>{drone.Dry_Weight__g_}</td>
-            <td>
-              <a
-                href={drone.field10}
-                target="_blank"
-                rel="noopener noreferrer"
-                title={drone.field10}
-              >
-                Link
-              </a>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <div className="field">
+        <div className="control">
+          <DebounceInput
+            minLength={0}
+            debounceTimeout={100}
+            className="input is-info"
+            type="text"
+            placeholder="Search"
+            onKeyDown={e => {
+              if (e.keyCode === 27) {
+                setSearchedValue('')
+                setIsFiltered(false)
+              }
+            }}
+            onChange={handleSearch}
+            value={searchedValue}
+          />
+        </div>
+      </div>
+      <table className="table">
+        <thead>
+          <TableHeadings />
+        </thead>
+        <tfoot>
+          <TableHeadings />
+        </tfoot>
+        <TableBody data={isFiltered ? filteredData : data} />
+      </table>
+    </>
   )
 }
